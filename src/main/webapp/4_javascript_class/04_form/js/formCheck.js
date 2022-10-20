5244464d830f20a3021f831daf2e8884895e744f
nononoh87 <112688283+nononoh87@users.noreply.github.com>
2022-10-20 16:34:32 +0900

    "WebUI - 2022.10.20"

--- src/main/webapp/4_javascript_class/04_form/js/formCheck.js
+++ src/main/webapp/4_javascript_class/04_form/js/formCheck.js
@@ -3,6 +3,18 @@ window.onload = function(){
 		var frm = document.getElementById('frm');
 		var inputs = document.querySelectorAll("input");
 		
-
+		frm.onsubmit = function(evt){
+			evt.stopPropagation(); //이벤트 전달 취소
+				
+			evt.preventDefault(); //이벤트 본래의 동작 취소
+			
+			//alert(frm.agree.checked);
+			if(!frm.agree.checked){
+				alert('반드시 케크해주셔야만 합니다.');
+				return;
+			}
+			
+			frm.onsubmit();
+		}
 
 	}
