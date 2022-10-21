5206a57e1c4c72f08bf71fc45fa37c5941998ddd
nononoh87 <112688283+nononoh87@users.noreply.github.com>
2022-10-21 18:03:22 +0900

    "WebUI - 2022.10.21"

--- src/main/webapp/4_javascript_class/99_practice/1_travel_agency/js/travel.js
+++ src/main/webapp/4_javascript_class/99_practice/1_travel_agency/js/travel.js
@@ -1,36 +1,60 @@
-window.onload = function(){
+window.onload = function() {
 
-  	var adult = document.getElementById('adult');
- 	var child = document.getElementById('child');
- 	var baby = document.getElementById('baby');
+	var adult = document.getElementById('adult');
+	var child = document.getElementById('child');
+	var infant = document.getElementById('infant');
+	adult.oninput = calc;
+	child.oninput = calc;
+	infant.oninput = calc;
 
-	adult.oninput = calc;	
-	child.oninput = calc;	
-	baby.oninput = calc;
-	
-	function calc(){
-		var total =document.getElementById('total');
-		total.value =adult.value* 39000 + child.value*29000 + baby.value*19000;	
+	function calc() {
+		document.getElementById('total').value = adult.value * 39000 + child.value * 29000 + infant.value * 19000 + '원';
 	}
-	var btn1 =document.getElementById(btn1);	
-	var btn2 =document.getElementById(btn2);
-	
-	btn1.onclick = agree1;	
-	btn2.onclick = agree2;
-	
-	var agree1 =document.getElementById(agree1);
-	var agree2 =document.getElementById(agree2);
-	
-	function agree(evt){
-		
-		if(!agree1.checked ||!agree.checked){
-		alert('반드시 체크해주셔야합니다');
-		return;
-		}alert('확인')
-		
+
+	var submit = document.getElementById('submit');
+
+	submit.onclick = function(evt) {
+		evt.preventDefault();
+		evt.stopImmediatePropagation();
+
+
+		if (!checkbox.checked) {
+			alert('반드시 여행약관에 동의하셔야 합니다.');
+			return;
+		}
+		if (!checkbox2.checked) {
+			alert('반드시 개인정보보호정책에 동의하셔야 합니다.');
+			return;
+		}
+		if (!radio1.checked && !radio2.checked) {
+			alert('반드시 성별을 체크하셔야 합니다.');
+			return;
+		}
+		if (adult.value == 0 && child.value == 0 && infant.value == 0) {
+			alert('반드시 0명 이상이셔야 합니다.');
+			return;
+		}
+		if (!koreanName.value) {
+			alert('한글 이름을 입력하셔야 합니다.');
+			return;
+		}
+		if (!englishName1.value || !englishName2.value) {
+			alert('영문 이름을 입력하셔야 합니다.');
+			return;
+		}
+		if (!jumin1.value || !jumin2.value) {
+			alert('주민번호를 입력하셔야 합니다.');
+			return;
+		}
+		if (!email.value) {
+			alert('이메일 주소를 입력하셔야 합니다.');
+			return;
+		}
+		if (!tel1.value || !tel2.value || !tel3.value) {
+			alert('전화번호를 바르게 입력하셔야 합니다.');
+			return;
+		}
+
+		alert("submitted successfully");
 	}
-	
-	
-
-
 }
